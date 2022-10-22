5bed469d8a84531ec1e3b1b899871ceef9f75829
JohanPP24 <johanpp29@gmail.com>
2022-10-22 11:25:09 +0700

    menambahkan file latihanwkwk.py

---
+++ latihanwkwk.py
@@ -0,0 +1,10 @@
+# Latihan wkwkwk
+print ("Program menentukan bilangan besar")
+a = input("Masukkan angka : ")
+b = input("Masukkan angka : ")
+if a > b:
+    max = a
+    print ("Bilangan terbesar= %c" %max)
+if b > a:
+    max = b
+    print("Error")
